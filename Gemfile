f501bec38d0d4f0eaf75456cb3c198642cb988db
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-10-27 19:59:47 +0000

    Bing Search implemented on /search

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'shotgun'
-gem 'google-search'
+gem 'bing-search'
--- server.rb
+++ server.rb
@@ -1,7 +1,17 @@
 require 'sinatra/base'
+require 'bing-search'
 
 class SearchAndMap < Sinatra::Base
+
   get '/' do
     'Hello World'
+  end
+
+  get '/search' do
+    BingSearch.account_key = ENV['THE_KEY']
+    BingSearch.web_only = true
+    results = BingSearch.web('BBC News', limit: 3)
+    website = results.first
+    p "#{website.title} #{website.description} #{website.url}"
   end
 end
