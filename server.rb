f437cac1b51edb7159d51a41b147ff5da714f447
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-10-28 00:15:01 +0000

    Added postcode parameter

--- server.rb
+++ server.rb
@@ -8,6 +8,7 @@ class SearchAndMap < Sinatra::Base
   end
 
   get '/search' do
+    @postcode = params[:postcode]
     BingSearch.account_key = ENV['THE_KEY']
     BingSearch.web_only = true
     @results = BingSearch.web(params[:search], limit: 3, adult: 'Strict', market: 'en-GB')
